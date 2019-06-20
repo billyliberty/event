7701e65dfd4f5aab18ab53e2d74b01288ffe445f
Joshua Crum <joshua.crum@gmail.com>
2019-06-20 15:13:35 +0000

    Updated to class methods.

--- app/models/event.rb
+++ app/models/event.rb
@@ -10,9 +10,13 @@ class Event
     @@all << self
   end
 
+  # Returns an array of all Events
+
   def self.all
     @@all
   end
+
+  # Returns an array of Attendees that have Tickets for the Event
 
   def tickets
     Ticket.all.select do |ticket|
@@ -26,6 +30,8 @@ class Event
     end
   end
 
+  # Returns the average age of all the Attendees that will be at the Event
+
   def total_crowd
     attendees.length
   end
@@ -36,14 +42,22 @@ class Event
     end
   end
 
-  def sell_ticket(attendee)
-    Ticket.new(attendee, self)
-  end
-
   def average_age
    total_age / total_crowd
   end
-
+  
+  # Accepts an Attendee instance as an argument and makes it so that, when #attendees is called on this Event instance, that Attendee instance will show up in the array.
+  
+  def sell_ticket(attendee)
+    Ticket.new(attendee, self)
+  end
+  
+  # Returns an integer that is the product of the number of Tickets sold for this event and the price of the Ticket.
+  
+  def money_made
+    total_crowd * self.ticket_price
+  end
+    
 end
 
 
